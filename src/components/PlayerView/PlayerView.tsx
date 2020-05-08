239d9461fd1892e95244f83ee433b6e82aaee7dd
vreezy117 <vreezy117@googlemail.com>
2020-05-08 23:13:57 +0200

    --- Auto-Git Commit ---

--- src/components/PlayerView/PlayerView.tsx
+++ src/components/PlayerView/PlayerView.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-// import { useState } from 'react';
+import { useRef, useEffect } from 'react';
+import player from '../../assets/player3.svg'; 
 
 import styles from './PlayerView.module.scss'; 
 // https://reactjs.org/docs/hooks-faq.html#how-can-i-do-data-fetching-with-hooks
@@ -12,16 +13,49 @@ export interface IPlayerView {
 
 
 export function PlayerView(props: IPlayerView) {
+    const playerSVG = useRef(null);
+    // return (
+    //   <div className={styles.player}>
+    //     {props.displayName}<br />
+    //     {props.wins}<br />
+    //     {/* {props.player.rpsWins}<br />
+    //     {props.player.active ? "active": "inactve"} */}
+    //     {/* {JSON.stringify(props.player.username)} */}
+    //   </div>
+    // );
+
+ 
+
+    useEffect(() => {
+      if(playerSVG) {
+        // const playerimage = document.getElementById();
+        // @ts-ignore
+        playerSVG.current.focus()
+        // if(playerimage !== null) {
+        //   // @ts-ignore
+        //   playerimage.getElementById("tspan2157").textContent = "Hallo Welt";
+        //   // playerlevel
+        //   // @ts-ignore
+        //   playerimage.getElementById("tspan2157-9").textContent = "1";
+        //   // playerwins
+        //   // @ts-ignore
+        //   playerimage.getElementById("tspan2157-9-9").textContent = "0";
+        // }
+      }
+    }, [playerSVG]) 
 
     return (
-      <div className={styles.player}>
-        {props.displayName}<br />
-        {props.wins}<br />
-        {/* {props.player.rpsWins}<br />
-        {props.player.active ? "active": "inactve"} */}
-        {/* {JSON.stringify(props.player.username)} */}
-      </div>
-    );
+      <div className="container">
+        <img src={player} className="player" id="playerimage" ref={playerSVG} alt="Player box"/>
+      {/* <svg >
+
+      </svg> */}
+     
+   <div className="char">
+       qefqefqef qefqe qefq
+   </div>
+  </div>
+    )
 
 
 
